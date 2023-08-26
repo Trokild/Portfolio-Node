d335b3a84c1b407ee63b43082c0fd4c20b60f33f
Torkild Sande <trokild@gmail.com>
2023-08-26 10:38:10 +0200

    Add handling new intro

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,5 @@
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 var express = require('express');
 var router = express.Router();
 const fs = require("fs")
@@ -7,6 +9,14 @@ const path = require("path")
 router.get('/', function(req, res, next) {
   let data = fs.readFileSync(path.resolve(__dirname, "../data/introArray.json"));
   res.render('home', { array: JSON.parse(data)});
+});
+
+router.post('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/introArray.json"));
+  let array = JSON.parse(rawdata);
+  const newArray = array.concat([req.body.newText])
+  fs.writeFileSync(path.resolve(__dirname, "../data/introArray.json"), JSON.stringify(newArray));
+  res.end();
 });
 
 module.exports = router;
